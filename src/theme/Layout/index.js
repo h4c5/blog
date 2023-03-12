44c9722f123a85dbfcea27b1c6b2d8299f1164dd
h4c5 <hakimcheikh@yahoo.fr>
2023-03-12 20:01:34 +0100

    fix: fix analytics

--- src/theme/Layout/index.js
+++ src/theme/Layout/index.js
@@ -1,12 +1,11 @@
 import React from "react";
 import Layout from "@theme-original/Layout";
-import { Analytics } from "@vercel/analytics/react";
 
 export default function LayoutWrapper(props) {
     return (
         <>
             <Layout {...props} />
-            <Analytics />
+            <script defer src="/_vercel/insights/script.js"></script>
         </>
     );
 }
